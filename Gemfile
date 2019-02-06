8e3b8ea63c5e5c6d5cb6e83bb01124fe55ea7981
Altair J Fernandes <tatosjb@gmail.com>
2019-02-06 17:41:59 -0200

    Configure doorkeeper

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'webpacker'
 gem 'active_model_serializers', '~>0.9.4'
 
 gem 'devise'
+gem 'doorkeeper'
 
 gem 'jbuilder', '~> 2.5'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users
 
-  root to: "rank#root"
+  root to: 'rank#root'
 
   namespace :api do
     namespace :v1 do
-     resources :scouts, only: :index
+      resources :scouts, only: :index
+
+      devise_for :users, controllers: {
+        sessions: 'api/v1/users/sessions'
+      }
     end
   end
 end
